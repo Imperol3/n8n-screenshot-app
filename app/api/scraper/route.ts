781b0cf8ac53aa90b1139c2427d990cbe1c51ad7
VictorKituku <victorkituku@gmail.com>
2025-03-20 12:21:11 +0300

    First Commit

---
+++ app/api/scraper/route.ts
@@ -0,0 +1,29 @@
+import chromium from "@sparticuz/chromium-min";
+import puppeteer from "puppeteer-core";
+
+// Optional: If you'd like to disable webgl, true is the default.
+chromium.setGraphicsMode = false;
+
+export async function POST() {
+  // Optional: Load any fonts you need. Open Sans is included by default in AWS Lambda instances
+  await chromium.font(
+    "https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf"
+  );
+
+  const isLocal = process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath:
+      process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://n8n.victorkituku.dev");
+  const pageTitle = await page.title();
+  await browser.close();
+
+  return Response.json({ Title: pageTitle });
+}
